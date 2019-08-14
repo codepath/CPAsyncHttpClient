425d25ed84baaff8c7c3f8430648905aac88d3e2
Roger Hu <roger.hu@gmail.com>
2019-08-13 22:08:45 -0700

    fix 0.0.4

--- library/src/main/java/com/codepath/asynchttpclient/RequestParams.java
+++ library/src/main/java/com/codepath/asynchttpclient/RequestParams.java
@@ -4,21 +4,21 @@ import java.util.HashMap;
 
 public class RequestParams extends HashMap<String, String> {
 
-    public void put(String key, Object value) {
-        if (key != null && value != null) {
-            this.put(key, value);
-        }
-    }
-
     public void put(String key, int value) {
         if (key != null) {
-            this.put(key, String.valueOf(value));
+            super.put(key, String.valueOf(value));
         }
     }
 
     public void put(String key, long value) {
         if (key != null) {
-            this.put(key, String.valueOf(value));
+            super.put(key, String.valueOf(value));
+        }
+    }
+
+    public void put(String key, boolean value) {
+        if (key != null) {
+            super.put(key, String.valueOf(value));
         }
     }
 
